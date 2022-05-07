b4fd0115267ba0546e7b18e039a807e859746443
Odebisi Solomon <odebisiidowu09@gmail.com>
2022-05-07 16:45:18 +0000

    Completed Project

--- index.js
+++ index.js
@@ -18,15 +18,33 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get("/api/:date?", (req, res) => {
+  const date = req.params.date;
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  if (date === undefined) {
+    res.json({ unix: new Date().getTime(), utc: new Date().toUTCString() });
+  } else if (date && date.split("-")[1] && new Date(date)!='Invalid Date') {
+    let dateObj = new Date(date);
+    if(dateObj.getTime()) {      
+    res.json({ unix: dateObj.getTime(), utc: dateObj.toUTCString() });
+    } else {
+      res.json({ error: "Invalid Date" });
+    }
+  } else if (new Date(+date)!='Invalid Date' && !isNaN(date)) {
+    let dateObj = new Date(Number(date));
+    res.json({ unix: +date, utc: dateObj.toUTCString() });
+  } else if (new Date(date) != "Invalid Date") {
+    let dateObj = new Date(date);
+    res.json({ unix: dateObj.getTime(), utc: dateObj.toUTCString() });
+  } 
+  else {
+    res.json({ error: "Invalid Date" });
+  }
 });
-
 
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
